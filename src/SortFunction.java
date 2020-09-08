1f49fcc5c8f74505039d48f0f02769d02cf99b0d
jatan-ops <58519071+jatan-ops@users.noreply.github.com>
2020-09-08 23:35:46 +0530

    With time measured

--- src/SortFunction.java
+++ src/SortFunction.java
@@ -2,19 +2,21 @@ import java.util.Arrays;
 
 public class SortFunction {
     public static void main(String[] args) {
-        long start  =System.currentTimeMillis();
+        long start  =System.nanoTime();
 
         Integer[] intArray = {20000,30,40,1407,9,600,1050,400,8006};
         sort(intArray);
 
-        long end  = System.currentTimeMillis();
-        System.out.println((end-start) + " : milliseconds");
+        long end  = System.nanoTime();
+        long timeElaspsed = end - start;
+        System.out.println(timeElaspsed/1000 + " : microseconds");
     }
 
     private static void sort(Integer[] intArray) {
 
         int[] newArray = new int[intArray.length];
 
+        //O(9)
         for (int i = 0; i<newArray.length; i++)
             newArray[i] = largestNumber(intArray);
 
--- src/SortFunctionSwap.java
+++ src/SortFunctionSwap.java
@@ -2,23 +2,25 @@ import java.util.Arrays;
 
 public class SortFunctionSwap {
     public static void main(String[] args) {
-        long start  =System.currentTimeMillis();
+        long start  =System.nanoTime();
 
         int[] intArray = {20000,30,40,1407,9,600,1050,400,8006};
         sort(intArray);
 
-        long end  = System.currentTimeMillis();
-        System.out.println((end-start) + " : milliseconds");
+        long end  = System.nanoTime();
+        long timeElaspsed = end - start;
+        System.out.println(timeElaspsed/1000 + " : microseconds");
     }
 
     private static void sort(int[] intArray) {
         int temp = 0;
+        //O[9^2]
         for(int i = 0; i < intArray.length;i++){
             for(int j = i+1; j < intArray.length; j++){
                 if(intArray[i] < intArray[j]){
-                    temp = intArray[j];
-                    intArray[j] = intArray[i];
-                    intArray[i] = temp;
+                    temp = intArray[j]; //O(1)
+                    intArray[j] = intArray[i]; //O(1)
+                    intArray[i] = temp; //O(1)
                 }
             }
         }
